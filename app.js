54fffcc806bc0e37ab2dd8004ace5479881a3eb5
Vitaliy <vitaltrachenko@gmail.com>
2016-12-18 23:20:59 +0200

    fix port

--- app.js
+++ app.js
@@ -4,4 +4,11 @@ app.get('/',function(req,res) {
   res.send("Hello World")
 });
 
-app.listen(3300);
+// app.listen(3300);
+
+var server = app.listen(3333, function () {
+  console.log('Server runing ...');
+});
+server.on('error', function(err) {
+  console.log(err);
+});
