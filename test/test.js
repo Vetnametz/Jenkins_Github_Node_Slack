e893ff6289cb858ebfd899ddcfec42b7feac7588
Vitaliy <vitaltrachenko@gmail.com>
2016-12-18 23:04:54 +0200

    fix port

--- test/test.js
+++ test/test.js
@@ -1,6 +1,6 @@
 var supertest = require("supertest")
   , should = require("should")
-  , server = supertest.agent("http://localhost:3000")
+  , server = supertest.agent("http://localhost:3000");
 
 describe("Server launch test",function(){
   it("should return landing page",function(done){
@@ -9,11 +9,11 @@ describe("Server launch test",function(){
     .expect("Content-type",/text/)
     .expect(200) 
     .end(function(err,res){
-      res.status.should.equal(200)
+      res.status.should.equal(200);
       done()
     })
   })
-})
+});
 
 
 
